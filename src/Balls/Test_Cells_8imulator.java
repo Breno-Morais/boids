9587f0906f2b21ea8faa8f4e5fefc9e1b461b630
Bergerghislain <92045695+Bergerghislain@users.noreply.github.com>
2023-11-13 17:45:23 +0100

    Update Test_Cells_8imulator.java

--- src/Balls/Test_Cells_8imulator.java
+++ src/Balls/Test_Cells_8imulator.java
@@ -8,6 +8,6 @@ public class Test_Cells_8imulator {
     public static void main(String[] args) {
         GUISimulator gui = new GUISimulator(500,500, Color.WHITE);
 
-        gui.setSimulable(new Cells(1000, 1000, gui));
+        gui.setSimulable(new Cells(500, 500, gui,2));
     }
 }
