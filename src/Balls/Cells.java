abac4c2c4efc2e5767a1f4e591d6a511464aaf27
Bergerghislain <92045695+Bergerghislain@users.noreply.github.com>
2023-11-13 17:44:23 +0100

    Update Cells.java

--- src/Balls/Cells.java
+++ src/Balls/Cells.java
@@ -3,41 +3,44 @@ import java.awt.*;
 import gui.GUISimulator;
 import gui.Rectangle;
 import gui.Simulable;
+import java.awt.Color;
 
 import static java.lang.Math.random;
 
 public class Cells implements Simulable {
-    private int rows;
-    private int cols;
-    private GUISimulator guiSim;
-    private int[][] grid;
+    protected int rows;
+    protected int cols;
+    protected GUISimulator guiSim;
+    protected int[][] grid;
+    protected int nbStates;
 
-    public Cells(int rows, int cols, GUISimulator guiSim) {
+    public Cells(int rows, int cols, GUISimulator guiSim, int nbStates) {
         this.rows = rows;
         this.cols = cols;
         this.guiSim = guiSim;
+        this.nbStates = nbStates;
         grid = new int[rows][cols];
-        initializeGrid();
+        initializeGrid(nbStates);
         draw();
     }
 
-    private void initializeGrid() {
+
+
+    public void initializeGrid(int nbStates) {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                grid[i][j] = (Math.random() < 0.2) ? 1 : 0; // Initialize randomly with 20% alive cells
+                grid[i][j] = (int) (Math.random() * (nbStates+2));// Initialize randomly 
             }
         }
     }
-
-    private void evolve() {
+    public void evolve() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                int neighbors = countAliveNeighbors(i, j);
+                int state =0;
+                int neighbors = countAliveNeighbors(i, j,state);
                 if ((grid[i][j] == 0) && (neighbors == 3)) {
-                    //System.out.println("VIVEU");
                     grid[i][j] = 1;
                 } else if ((grid[i][j] == 1) && !(neighbors == 2 || neighbors == 3)) {
-                    //System.out.println("MORREU");
                     grid[i][j] = 0;
                 }
             }
@@ -46,13 +49,13 @@ public class Cells implements Simulable {
 
     }
 
-    private int countAliveNeighbors(int x, int y) {
+    public int countAliveNeighbors(int x, int y,int state) {
         int count = 0;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 int neighborX = (x + i + rows) % rows;
                 int neighborY = (y + j + cols) % cols;
-                if (!(i == 0 && j == 0) && (grid[neighborX][neighborY] == 1)) {
+                if (!(i == 0 && j == 0) && (grid[neighborX][neighborY] == state)){
                     count++;
                 }
             }
@@ -70,18 +73,31 @@ public class Cells implements Simulable {
     public void restart() {
 
     }
+    protected Color getColorForState(int state) {
+        switch (state) {
+            case 0:
+                return Color.WHITE;
+            case 1:
+                return Color.BLUE;
 
-    private void draw() {
+        }
+        return null;
+    }
+    protected void draw() {
         guiSim.reset();    // clear the window
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                if(grid[i][j] == 1)
-                    guiSim.addGraphicalElement(new Rectangle(i*5, j*5, Color.BLUE, Color.BLUE, 5));
+                // Color mapping based on the state
+                Color color = getColorForState(grid[i][j]);
+                guiSim.addGraphicalElement(new Rectangle(i*10, j*10, color, color, 10));
+
             }
         }
     }
 }
+
+
 
 
 
